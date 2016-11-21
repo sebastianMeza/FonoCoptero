cefbd1afd115a7899c2033cbb237dc254ddab71e
sebastianMeza <sebastian.meza@usach.cl>
2016-11-21 17:09:38 -0300

    se reconoce gps y sensores

--- app/src/main/java/cl/tanmeza/fonocoptero/PosRotSensors.java
+++ app/src/main/java/cl/tanmeza/fonocoptero/PosRotSensors.java
@@ -12,14 +12,22 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.PermissionChecker;
 import android.util.Log;
+import android.view.Display;
+import android.view.WindowManager;
+import android.widget.Toast;
 
-public class PosRotSensors extends Activity implements SensorEventListener{
+public class PosRotSensors extends Activity implements LocationListener, SensorEventListener{
 
     private SensorManager sensorManager;
+    private PowerManager mPowerManager;
+    private WindowManager mWindowManager;
+    private Display mDisplay;
+
     private Sensor rotationSensor, pressureSensor;
     private HeliState heliState;
     private float[] rotationVec, rotationMatrix, yawPitchRollVec;
@@ -36,7 +44,6 @@ public class PosRotSensors extends Activity implements SensorEventListener{
     public static final float ALTITUDE_SMOOTHING = 0.95f;
     public static final double EARTH_RADIUS = 6371000; // [m].
     public static final boolean USE_GPS = false;
-
     Context mContext;
 
     public PosRotSensors(Context mContext) {
@@ -53,91 +60,81 @@ public class PosRotSensors extends Activity implements SensorEventListener{
         // Get the sensors manager.
         sensorManager = (SensorManager) mContext.getSystemService(SENSOR_SERVICE);
 
+        if(sensorManager.getSensorList(Sensor.TYPE_ROTATION_VECTOR).size()!=0){
+            rotationSensor = sensorManager.getSensorList(Sensor.TYPE_ROTATION_VECTOR).get(0);
+            sensorManager.registerListener(this,rotationSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if(sensorManager.getSensorList(Sensor.TYPE_PRESSURE).size()!=0){
+            pressureSensor = sensorManager.getSensorList(Sensor.TYPE_PRESSURE).get(0);
+            sensorManager.registerListener(this,pressureSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        //mPowerManager = (PowerManager) getSystemService(POWER_SERVICE);
+        /*
+
         // Get the sensors.
         rotationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
         pressureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        */
 
         Log.v("Fonotest", "paso 3");
         if((rotationSensor == null) || (pressureSensor == null)) {
-            Log.v("Fonotest", "Falta un sensor!");
+            Log.v("Fonotest", "Sensores faltantes!");
         }
         else{
-            Log.v("Fonotest",
-                    "Sensor rota: "+rotationSensor.getName()+
-                    ", Sensor press: "+pressureSensor.getName());
+            Log.v("Fonotest", "Sensor rota: "+rotationSensor.getName()+ ", Sensor press: "+pressureSensor.getName());
         }
 
-        // Get the GPS manager.
+        //insert HERE
+
+        //Log.v("Fonotest", "GPS:"+USE_GPS);
+
+        /*
         if(USE_GPS) {
             locationManager = (LocationManager) getSystemService(mContext.LOCATION_SERVICE);
             Log.v("Fonotest", "paso 4 gps");
         }
-
-        locationListener = new LocationListener(){
-            public void onLocationChanged(Location location){
-                heliState.gpsElevation = (float) location.getAltitude();
-                heliState.gpsAccuracy = location.getAccuracy();
-                heliState.longitude = location.getLongitude();
-                heliState.latitude = location.getLatitude();
-
-                heliState.nSatellites = (Integer) location.getExtras().get("satellites");
-
-                Log.v("Fonotest", "GPS: " + "altitud: " + heliState.gpsElevation + ", "
-                        + "exactitud: " + heliState.gpsAccuracy + ", "
-                        + "longitud: " + heliState.longitude + ", "
-                        + "latitud: " + heliState.latitude + ", "
-                        + "navegacion: " + location.getBearing() + ", "
-                        + "velocidad: " + location.getSpeed() + ", "
-                        + "proveedor: " + location.getProvider());
-
-                // Convert longitude+latitude to x+y (using the small angles
-                // approximation: sin(x)~=x).
-                heliState.xPos = (float) (EARTH_RADIUS * (heliState.longitude-longitudeZero) * DEG_TO_RAD);
-                heliState.yPos = (float) (EARTH_RADIUS * (heliState.latitude-latitudeZero) * DEG_TO_RAD);
-
-                // Convert heading+speed to speedx+speedy.
-                heliState.xSpeed = location.getSpeed() * (float) Math.cos(location.getBearing());
-                heliState.ySpeed = location.getSpeed() * (float) Math.sin(location.getBearing());
+        */
+        locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+        //locationManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 3000, 10, this);
+        if (locationManager != null){
+            if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                    || ActivityCompat.checkSelfPermission(mContext,Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+                Log.v("Fonotest", "GPS configurado");
             }
+        }
 
-            public void onStatusChanged(String provider, int status, Bundle extras)
-            {
-                Log.v("Fonotest", "sensor change status");
-
-                heliState.nSatellites = (Integer) extras.get("satellites");
-                heliState.gpsStatus = status;
-            }
-
-            public void onProviderEnabled(String provider) {}
-            public void onProviderDisabled(String provider){}
-        };
-        Log.v("Fonotest", "paso 5");
-
-        if(USE_GPS){
-            if (locationManager != null) {
-                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-                        || ActivityCompat.checkSelfPermission(getApplicationContext(),Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-                    }
-                }
-            }
-        Log.v("Fonotest", "paso final");
 
     }
 
-
-
-    protected void onResume(){
-        Log.v("Fonotest", "resume sensor");
+    @Override
+    protected void onResume() {
         super.onResume();
-        sensorManager.registerListener(this, rotationSensor, SensorManager.SENSOR_DELAY_FASTEST);
-        sensorManager.registerListener(this, pressureSensor, SensorManager.SENSOR_DELAY_FASTEST);
+
+        if(sensorManager.getSensorList(Sensor.TYPE_ROTATION_VECTOR).size()!=0){
+            rotationSensor = sensorManager.getSensorList(Sensor.TYPE_ROTATION_VECTOR).get(0);
+            sensorManager.registerListener(this,rotationSensor, SensorManager.SENSOR_DELAY_NORMAL); //SENSOR_DELAY_NORMAL o SENSOR_DELAY_FASTEST
+        }
+        if(sensorManager.getSensorList(Sensor.TYPE_PRESSURE).size()!=0){
+            pressureSensor = sensorManager.getSensorList(Sensor.TYPE_PRESSURE).get(0);
+            sensorManager.registerListener(this,pressureSensor, SensorManager.SENSOR_DELAY_NORMAL);
         }
 
+    }
+
+    @Override
+    protected void onPause() {
+        sensorManager.unregisterListener(this);
+        super.onPause();
+    }
+
+
+/*
     @Override
     protected void onPause(){
-        Log.v("Fonotest", "pausa sensor");
         super.onPause();
+        Log.v("Fonotest", "pausa sensor");
         // Disable the GPS.
         if(USE_GPS)
 
@@ -152,23 +149,78 @@ public class PosRotSensors extends Activity implements SensorEventListener{
         // Disable the inertial sensors.
         sensorManager.unregisterListener(this);
         }
+*/
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy){
-        Log.v("Fonotest", "accu sensor");
-        // These sensors should not change precision.
-        }
+    public void onLocationChanged(Location location){
+        Log.v("Fonotest", "GPS change");
+
+        heliState.gpsElevation = (float) location.getAltitude();
+        heliState.gpsAccuracy = location.getAccuracy();
+        heliState.longitude = location.getLongitude();
+        heliState.latitude = location.getLatitude();
+
+        heliState.nSatellites = location.getExtras().getInt("satellites");
+
+        String str = "GPS: " + "altitud: " + heliState.gpsElevation + ", "
+                + "exactitud: " + heliState.gpsAccuracy + ", "
+                + "longitud: " + heliState.longitude + ", "
+                + "latitud: " + heliState.latitude + ", "
+                + "nºSatelites: " + heliState.nSatellites;// + ", "
+
+                /*+ "navegacion: " + location.getBearing() + ", "
+                + "velocidad: " + location.getSpeed() + ", "
+                + "proveedor: " + location.getProvider();*/
+
+        // Convert longitude+latitude to x+y (using the small angles
+        // approximation: sin(x)~=x).
+        heliState.xPos = (float) (EARTH_RADIUS * (heliState.longitude-longitudeZero) * DEG_TO_RAD);
+        heliState.yPos = (float) (EARTH_RADIUS * (heliState.latitude-latitudeZero) * DEG_TO_RAD);
+
+        // Convert heading+speed to speedx+speedy.
+        heliState.xSpeed = location.getSpeed() * (float) Math.cos(location.getBearing());
+        heliState.ySpeed = location.getSpeed() * (float) Math.sin(location.getBearing());
+
+        //str = "Latitude: "+location.getLatitude()+", Longitude: "+location.getLongitude();
+        Toast.makeText(mContext, str, Toast.LENGTH_LONG).show();
+    }
+
+
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        /******** se llama cuando el Gps esta apagado *********/
+        Toast.makeText(mContext, "GPS apagado!", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        /******** se llama cuando el Gps esta prendido *********/
+        Toast.makeText(mContext, "GPS prendido", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras){
+        Log.v("Fonotest", "GPS sensor change status");
+        heliState.nSatellites = extras.getInt("satellites");
+        heliState.gpsStatus = status;
+    }
+
 
     @Override
     public void onSensorChanged(SensorEvent event){
-        Log.v("Fonotest", "on sensor");
+        Log.v("Fonotest", "lectura de sensores.");
 
-        if(event.sensor == rotationSensor)
-        {
+        if (event.sensor == rotationSensor){
             // Get the time and the rotation vector.
             heliState.time = event.timestamp;
             System.arraycopy(event.values, 0, rotationVec, 0, 3);
-            Log.v("Fonotest", "sensor_rotation 1");
+            //Log.v("Fonotest", "sensor_rotation 1");
 
             // Convert the to "yaw, pitch, roll".
             SensorManager.getRotationMatrixFromVector(rotationMatrix, rotationVec);
@@ -181,17 +233,17 @@ public class PosRotSensors extends Activity implements SensorEventListener{
 
             // New sensors data are ready.
             newMeasurementsReady = true;
-            Log.v("Fonotest", "sensor_rotation 2");
+            //Log.v("Fonotest", "sensor_rotation 2");
 
         }
         else if(event.sensor == pressureSensor)
         {
-            Log.v("Fonotest", "sensor_presion 1");
+            //Log.v("Fonotest", "sensor_presion 1");
             float pressure = event.values[0];
             float rawAltitudeUnsmoothed = SensorManager.getAltitude(SensorManager.PRESSURE_STANDARD_ATMOSPHERE, pressure);
             absoluteElevation = (absoluteElevation*ALTITUDE_SMOOTHING) + (rawAltitudeUnsmoothed*(1.0f-ALTITUDE_SMOOTHING));
             heliState.baroElevation = absoluteElevation - elevationZero;
-            Log.v("Fonotest", "sensor_presion 2");
+            //Log.v("Fonotest", "sensor_presion 2");
         }
     }
 
@@ -231,7 +283,6 @@ public class PosRotSensors extends Activity implements SensorEventListener{
             angle += 360.0f;
         while(angle > 180.0f)
             angle -= 360.0f;
-
         return angle;
         }
 
